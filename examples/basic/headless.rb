400cb079aca2fb63e7aa6275cd2d3d333e15c18b
lmo0 <luis.mayorga@nreca.coop>
2016-08-24 15:54:25 -0400

    Update Selenium to use MiniTest

--- examples/basic/headless.rb
+++ examples/basic/headless.rb
@@ -1,24 +1,20 @@
 require 'selenium-webdriver'
+require 'minitest/autorun'
 require 'rspec/expectations'
 
-include RSpec::Matchers
-
+class AmazonTest < MiniTest::Test
   def setup
     @driver = Selenium::WebDriver.for(:remote, :url => "http://localhost:32768/wd/hub", :desired_capabilities => :firefox)
+  end
+
+  def test_post
+    @driver.get 'http://www.amazon.com'
+    expect(@driver.title).to eql 'Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more'
+    @driver.save_screenshot 'amazon.png'
   end
 
   def teardown
     @driver.quit
   end
 
-  def run
-  setup
-  yield
-  teardown
-  end
-
-  run do 
-   @driver.get 'http://www.amazon.com'
-   expect(@driver.title).to eql 'Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more'
-   @driver.save_screenshot 'amazon.png'
-  end
+end
