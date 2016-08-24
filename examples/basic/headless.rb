f966971f8ee5ed9d397a5a6141d99550c5655a89
lmo0 <luis.mayorga@nreca.coop>
2016-08-24 16:00:18 -0400

    Fix assert_equal

--- examples/basic/headless.rb
+++ examples/basic/headless.rb
@@ -9,7 +9,7 @@ class AmazonTest < MiniTest::Test
 
   def test_post
     @driver.get 'http://www.amazon.com'
-    expect(@driver.title).to eql 'Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more'
+    assert_equal(@driver.title, "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more")
     @driver.save_screenshot 'amazon.png'
   end
 
